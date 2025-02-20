2f64d738e544e051e8936ae77c77dd07cd797bab
tinoryj <tinoryj@gmail.com>
2025-02-21 02:12:54 +0800

    update for batch processing

---
+++ Analysis/runCountOp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PATH_TO_FIRST_PART=/home/jzhao/geth-trace-2025-02-11-19-18-38
+PATH_TO_SECOND_PART=/mnt/ssd/geth-trace-2025-02-13-15-33-09
+
+num_blocks_in_each_run=100000
+start_block_id=20500000
+end_block_id=20600000
+
+for i in {0..3}
+do
+    echo "Run $i-th part"
+    mkdir -p ./countKVDis-$i-block-$start_block_id-$end_block_id
+    ./countOpDistribution $PATH_TO_FIRST_PART ./countKVDis.txt 0 10000 $start_block_id $end_block_id
+    start_block_id=$((start_block_id+num_blocks_in_each_run))
+    end_block_id=$((end_block_id+num_blocks_in_each_run))
+    mv *.txt ./countKVDis-$i-block-$start_block_id-$end_block_id
+done
+
+start_block_id=20700000
+end_block_id=20800000
+
+for i in {3..9}
+do
+    echo "Run $i-th part"
+    mkdir -p ./countKVDis-$i-block-$start_block_id-$end_block_id
+    ./countOpDistribution $PATH_TO_SECOND_PART ./countKVDis.txt 0 10000 $start_block_id $end_block_id
+    start_block_id=$((start_block_id+num_blocks_in_each_run))
+    end_block_id=$((end_block_id+num_blocks_in_each_run))
+    mv *.txt ./countKVDis-$i-block-$start_block_id-$end_block_id
+done
