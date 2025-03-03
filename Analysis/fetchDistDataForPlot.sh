7b716450bfbd099e70592cecdff2afdf5f14fd56
tinoryj <tinoryj@gmail.com>
2025-03-04 01:00:19 +0800

    Update scripts

--- Analysis/fetchDistDataForPlot.sh
+++ Analysis/fetchDistDataForPlot.sh
@@ -3,16 +3,16 @@
 PATH_TO_WITHOUT_CACHE=/mnt/16T/GethResults/KVOpDistWithoutCache/mergedDistribution
 PATH_TO_WITH_CACHE=/mnt/16T/GethResults/KVOpDistWithCache/mergedDistribution
 
-TargetDataTypes=("SnapshotNodeAccountPrefix" "SnapshotNodeStoragePrefix" "TrieNodeAccountPrefix" "TrieNodeStoragePrefix" "TxLookupPrefix")
+TargetDataTypes=("SnapshotAccountPrefix" "SnapshotStoragePrefix" "TrieNodeAccountPrefix" "TrieNodeStoragePrefix" "TxLookupPrefix")
 TargetOpTypes=("get" "put" "batchput" "delete")
 TargetDrawPonitNumbers=(1000 10000 50000)
 
 for TargetDataType in "${TargetDataTypes[@]}"; do
     for TargetOpType in "${TargetOpTypes[@]}"; do
         for TargetDrawPonitNumber in "${TargetDrawPonitNumbers[@]}"; do
-            echo "Processing ${TargetDataType}..."
+            echo "Processing ${TargetDataType} ${TargetOpType} ${TargetDrawPonitNumber}"
             filePathWithoutCache="${PATH_TO_WITHOUT_CACHE}/${TargetDataType}_${TargetOpType}_without_key_dis.txt"
-            filePathWithCache="${PATH_TO_WITH_CACHE}/${TargetDataType}_${TargetOpType}_with_key_dis.txt"
+            filePathWithCache="${PATH_TO_WITH_CACHE}/${TargetDataType}_${TargetOpType}_without_key_dis.txt"
             if [ -f "$filePathWithCache" ]; then
                 outputFileName="${TargetDataType}_${TargetOpType}_sample_${TargetDrawPonitNumber}_withCache.txt"
                 python3 generateDistDataForPlot.py "${filePathWithCache}" "${TargetDrawPonitNumber}" "${outputFileName}"
